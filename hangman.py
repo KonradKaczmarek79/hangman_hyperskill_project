29eb5f549042ad0a3b3f7b8b82b3a51eea3875fa
Konrad Kaczmarek <55359118+KonradKaczmarek79@users.noreply.github.com>
2025-10-18 17:01:16 +0200

    improve correct user answers handling

--- hangman.py
+++ hangman.py
@@ -6,12 +6,17 @@ class HangmanGame:
     initial_message = 'H A N G M A N  # 8 attempts'
     input_message = 'Input a letter: '
     stop_message = 'Thanks for playing!'
+    wins_lost_messages = {
+        0: "\nYou lost!",
+        1: "You guessed the word!\nYou survived!",
+    }
 
     def __init__(self, attempts: int):
         self.secret_word: str = random.choice(HangmanGame.secret_words)
         self.shadowed_secret_word = "-" * len(self.secret_word)
         self.attempts = attempts
         self.letters_position = {}
+        self.user_wins = 0
 
         self.__get_letters_position()
 
@@ -21,21 +26,35 @@ class HangmanGame:
 
     def play(self):
         print(HangmanGame.initial_message, end='\n\n')
+        print(self.shadowed_secret_word, end='\n')
 
         while self.attempts > 0:
-            self.attempts -= 1
-            print(self.shadowed_secret_word)
-            self.single_move(input(HangmanGame.input_message))
-            print()
-            if self.attempts < 1:
-                print(HangmanGame.stop_message)
 
-    def single_move(self, letter):
+            correct_answer = self.single_move(input(HangmanGame.input_message))
+
+            if self.secret_word == self.shadowed_secret_word:
+                self.user_wins = 1
+                self.attempts = 0
+
+            if not correct_answer:
+                self.attempts -= 1
+
+            if self.attempts <= 0:
+
+                message_prefix = f'\n{self.shadowed_secret_word}\n' if '-' not in self.shadowed_secret_word else ''
+                print(f'{message_prefix}{HangmanGame.wins_lost_messages.get(self.user_wins)}')
+            else:
+                print(f'\n{self.shadowed_secret_word}', end='\n')
+
+    def single_move(self, letter) -> bool:
         letter = letter.lower()
         found_index = self.secret_word.find(letter)
+        result = False
 
         if found_index == -1:
             print(f"That letter doesn't appear in the word.")
+        elif letter in self.shadowed_secret_word:
+            print("No improvements.")
         else:
             for index in self.letters_position[letter]:
                 if index < len(self.shadowed_secret_word) - 1:
@@ -43,6 +62,8 @@ class HangmanGame:
                                                  f'{self.shadowed_secret_word[index + 1:]}')
                 else:
                     self.shadowed_secret_word = f'{self.shadowed_secret_word[:index]}{letter}'
+            result = True
+        return result
 
 
 if __name__ == '__main__':
