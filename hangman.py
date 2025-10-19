20afcade987d1593424e32d4b8f7e021b24430e4
Konrad Kaczmarek <55359118+KonradKaczmarek79@users.noreply.github.com>
2025-10-19 18:36:58 +0200

    add additional validation of the entered character format

--- hangman.py
+++ hangman.py
@@ -1,22 +1,74 @@
 import random
+import re
+
+class HangmanRules:
+    def __init__(self, pattern=r'[a-z]', required_length=1):
+        self.input_pattern = re.compile(pattern)
+        self.required_length = required_length
+
+    def check_letter_correctness(self, letter) -> bool:
+        """
+        By default, checks that the player entered a lowercase English letter.
+        Or if it agrees with any other pattern you passed into __init__.
+
+        :param letter: letter to check
+        """
+        if self.input_pattern.match(letter) is None:
+            print('Please, enter a lowercase letter from the English alphabet.')
+            return False
+        return True
+
+    def check_if_single_letter(self, letter) -> bool:
+        """ Checks whether players enter exactly one letter. """
+        if len(letter) != self.required_length:
+            print('Please, input a single letter.')
+            return False
+        return True
+
+    @staticmethod
+    def letter_not_repeated(letter, reference_iterable: set | list | tuple | str) -> bool:
+        """ Checks whether a letter is repeated. """
+        result = True
+        if letter in reference_iterable:
+            print("You've already guessed this letter.")
+            result = False
+        return result
+
+    def check_input_correctness(self, letter, reference_iterable):
+        """
+        Launches user input correctness checking.
+
+        Uses methods defined in HangmanRules (above).
+        None of the three errors described above should reduce the number of remaining attempts!
+
+        :param letter: letter to check
+        :param reference_iterable: iterable of letters to check
+
+        :return: True if all letters are correct, False otherwise
+        """
+        return (self.check_if_single_letter(letter)
+                and self.check_letter_correctness(letter)
+                and self.letter_not_repeated(letter, reference_iterable))
 
 
-class HangmanGame:
+class HangmanGame(HangmanRules):
     secret_words = ['python', 'java', 'swift', 'javascript']
     initial_message = 'H A N G M A N  # 8 attempts'
     input_message = 'Input a letter: '
     stop_message = 'Thanks for playing!'
     wins_lost_messages = {
         0: "\nYou lost!",
-        1: "You guessed the word!\nYou survived!",
+        1: "You guessed the word <PLACEHOLDER>!\nYou survived!",
     }
 
-    def __init__(self, attempts: int):
+    def __init__(self, attempts: int, input_pattern=r'[a-z]'):
+        super().__init__(input_pattern)
         self.secret_word: str = random.choice(HangmanGame.secret_words)
         self.shadowed_secret_word = "-" * len(self.secret_word)
         self.attempts = attempts
         self.letters_position = {}
         self.user_wins = 0
+        self.letters_used = set()
 
         self.__get_letters_position()
 
@@ -29,7 +81,6 @@ class HangmanGame:
         print(self.shadowed_secret_word, end='\n')
 
         while self.attempts > 0:
-
             correct_answer = self.single_move(input(HangmanGame.input_message))
 
             if self.secret_word == self.shadowed_secret_word:
@@ -42,17 +93,23 @@ class HangmanGame:
             if self.attempts <= 0:
 
                 message_prefix = f'\n{self.shadowed_secret_word}\n' if '-' not in self.shadowed_secret_word else ''
-                print(f'{message_prefix}{HangmanGame.wins_lost_messages.get(self.user_wins)}')
+                print(self.__return_stop_message(message_prefix))
             else:
                 print(f'\n{self.shadowed_secret_word}', end='\n')
 
     def single_move(self, letter) -> bool:
-        letter = letter.lower()
-        found_index = self.secret_word.find(letter)
         result = False
 
+        if not self.check_input_correctness(letter, self.letters_used):
+            return True
+
+        # add letter to letters_used set for check it in further moves
+        self.letters_used.add(letter)
+
+        found_index = self.secret_word.find(letter)
+
         if found_index == -1:
-            print(f"That letter doesn't appear in the word.")
+            print(f"That letter doesn't appear in the word.  # {self.attempts - 1} attempts")
         elif letter in self.shadowed_secret_word:
             print("No improvements.")
         else:
@@ -64,6 +121,12 @@ class HangmanGame:
                     self.shadowed_secret_word = f'{self.shadowed_secret_word[:index]}{letter}'
             result = True
         return result
+
+    def __return_stop_message(self, message_prefix):
+        msg = f'{message_prefix}{HangmanGame.wins_lost_messages.get(self.user_wins)}'
+        if '<PLACEHOLDER>' in msg:
+            msg = msg.replace('<PLACEHOLDER>', self.secret_word)
+        return msg
 
 
 if __name__ == '__main__':
